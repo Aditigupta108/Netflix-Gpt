39ee4339fe38f9f3d6da4eb11cd17d79b7643be9
Aditigupta108 <121371951+Aditigupta108@users.noreply.github.com>
2024-01-09 14:05:20 +0530

    cra & tailwind config

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-2xl bg-slate-400 m-4 p-5 text-red-900 ">
+    hello world lets begin building netflix gpt
+    
     </div>
   );
 }
